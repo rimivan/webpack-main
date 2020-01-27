e43b5dd4673fdccee4cd095849183cccca43d872
rimivan <rimola7@gmail.com>
2020-01-27 16:06:27 +0100

    Updated webpack config with plugins
    Added plugins:
    * HtmlWebpackPlugin
    * JavascriptObfuscator Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+const JavaScriptObfuscator = require('webpack-obfuscator');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: "./index.js",
     output: {
       filename: "bundle.js",
       chunkFilename: '[name].bundle.js',
     },
+    plugins: [
+      new JavaScriptObfuscator ({
+        rotateUnicodeArray: true
+      }, ['excluded_bundle_name.js']),
+      new HtmlWebpackPlugin({template: './index.html'})
+    ],
     mode: 'development',
     watch: true,
     devServer: {
